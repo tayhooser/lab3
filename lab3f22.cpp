c2b383da6af34ad5fa5fe522cf33af81a2427857
tayhooser <84413327+tayhooser@users.noreply.github.com>
2022-09-06 08:42:38 -0700

    coding style fixes

--- lab3f22.cpp
+++ lab3f22.cpp
@@ -73,76 +73,86 @@
 const int MAX_READ_ERRORS = 100;
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	//Function prototypes.
 	BIO *ssl_setup_bio(void);
 	void show_cert_data(SSL *ssl, BIO *outbio, const char *hostname);
 	void set_to_non_blocking(const int sock);
+	
 	//Data variables.
 	int sd;
-    struct hostent *host;
-    struct sockaddr_in addr;
-    BIO *outbio = NULL;
-    //SSL_METHOD *method;
-    SSL_CTX *ctx;
-    SSL *ssl;
-    char req[1000];
-    int req_len;
+	struct hostent *host;
+	struct sockaddr_in addr;
+	BIO *outbio = NULL;
+	
+	//SSL_METHOD *method;
+	SSL_CTX *ctx;
+	SSL *ssl;
+	char req[1000];
+	int req_len;
+	
 	//Default values for command-line input
 	char hostname[256] = "www.google.com";
 	char pagename[256] = "";
-    int port = PORT;
+	int port = PORT;
 	int ret;
+	
 	//Get any command-line arguments.
 	if (argc > 1)
 		strcpy(hostname, argv[1]);
 	if (argc > 2)
 		strcpy(pagename, argv[2]);
-	//
+
 	//Setup the SSL BIO
 	outbio = ssl_setup_bio();
+	
 	//Initialize the SSL library
-    if(SSL_library_init() < 0) BIO_printf(outbio, "Could not initialize the OpenSSL library !\n");
-	//
+	if (SSL_library_init() < 0) {
+		BIO_printf(outbio, "Could not initialize the OpenSSL library !\n");
+	}
+
 	//method pointer was not a constant, and causing a serious error,
 	//so method was defined here as a constant.
 	const SSL_METHOD *method = SSLv23_client_method();
-	//
+
 	//Setup a context.
 	//A context is used to store and work with data elements of OpenSSL.
 	ctx = SSL_CTX_new(method);
-    SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
-	//
+	SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
+
 	//next 2 lines of code are not currently needed.
 	//SSL_MODE_AUTO_RETRY flag of the SSL_CTX_set_mode call.
 	//SSL_CTX_set_mode(ctx, SSL_MODE_AUTO_RETRY);
 	//
 	//Setup the socket used for connection.
-    host = gethostbyname(hostname);
-    sd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = *(long*)(host->h_addr);
-    if (connect(sd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        BIO_printf(outbio, "%s: Cannot connect to host %s [%s] on port %d.\n", argv[0], hostname, inet_ntoa(addr.sin_addr), port);
-    }
-	//
+	host = gethostbyname(hostname);
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = *(long*)(host->h_addr);
+	if (connect(sd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+		BIO_printf(outbio, "%s: Cannot connect to host %s [%s] on port %d.\n", 
+			argv[0], hostname, inet_ntoa(addr.sin_addr), port);
+	}
+
 	//Connect using the SSL certificate.
-    ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, sd);
-    SSL_connect(ssl);
-	//
+	ssl = SSL_new(ctx);
+	SSL_set_fd(ssl, sd);
+	SSL_connect(ssl);
+
 	//Show the certificate data
 	show_cert_data(ssl, outbio, hostname);
-	//
+
 	//A non-blocking socket will make the ssl_read() not block.
 	set_to_non_blocking(sd);
-	//
+
 	//Send the http request to the host server.
-	sprintf(req, "GET /%s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n", pagename, hostname, USERAGENT);    
-    req_len = strlen(req);
-    ret = SSL_write(ssl, req, req_len);
+	sprintf(req, "GET /%s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n", 
+		pagename, hostname, USERAGENT);    
+	req_len = strlen(req);
+	ret = SSL_write(ssl, req, req_len);
     
 //---------------------------------------------------------------------
 //Fixed warning: this 'if' clause does not guard... [-Wmisleading-indentation]
@@ -154,22 +164,23 @@ int main(int argc, char *argv[]){
 	if (ret <= 0) {
 		fprintf(stderr, "ERROR: SSL_write\n"); fflush(stderr);
 	}
-	//
+	
 	//The request was sent, not the data is read...
 	void read_the_page(SSL *ssl);
 	read_the_page(ssl);
-	//
+
 	//Cleanup dynamically allocated data.
-    SSL_free(ssl);
-    close(sd);
-    SSL_CTX_free(ctx);
-	//
+	SSL_free(ssl);
+	close(sd);
+	SSL_CTX_free(ctx);
+
 	return 0;
 }
 
-void read_the_page(SSL *ssl){
-    int bytes, nreads, nerrs;
-    char buf[256];
+void read_the_page(SSL *ssl)
+{
+	int bytes, nreads, nerrs;
+	char buf[256];
 	
 //---------------------------------------------------------------------
 //Fixed warning: unused variable 'ret' [-Wunused-variable]
@@ -183,36 +194,41 @@ void read_the_page(SSL *ssl){
 	//Start with an error condition.
 	bytes = -1;
 	memset(buf, '\0', sizeof(buf));
-	while(bytes <= 0){
+	while (bytes <= 0) {
 	    bytes = SSL_read(ssl, buf, sizeof(buf));
 		//A slight pause can cause fewer reads to be needed.
 		usleep(10000);
 	}
+	
 	//A successful priming read was accomplished.
 	//Now read all the data.
 	nreads = 1;
 	//Allow for some read errors to happen, while getting the complete data.
 	nerrs = 0;
-	while(bytes >= 0 && nerrs < MAX_READ_ERRORS){
-        write(STDOUT_FILENO, buf, bytes);
-        memset(buf, '\0', sizeof(buf));
+	while (bytes >= 0 && nerrs < MAX_READ_ERRORS) {
+		write(STDOUT_FILENO, buf, bytes);
+		memset(buf, '\0', sizeof(buf));
 		++nreads;
-        bytes = SSL_read(ssl, buf, sizeof(buf));
-		if (bytes == 0) ++nerrs; else nerrs = 0;
+		bytes = SSL_read(ssl, buf, sizeof(buf));
+		if (bytes == 0) 
+			++nerrs; 
+		else 
+			nerrs = 0;
 		//A slight pause can cause fewer reads to be needed.
 		//usleep argument is in millionths of a second.
 		//This pause is 20,000/1,000,000 or 2-hundredths of a second.
 		usleep(20000);
-    }
+	}
 	printf("\nn calls to ssl_read(): %i\n", nreads); fflush(stdout);
 	return;
 }
 
-BIO *ssl_setup_bio(void){
+BIO *ssl_setup_bio(void)
+{
 	//Setup the ssl BIO, basic I/O abstraction.
 	//https://www.openssl.org/docs/man1.1.0/man3/bio.html
-    BIO *bio = NULL;
-    OpenSSL_add_all_algorithms();
+	BIO *bio = NULL;
+	OpenSSL_add_all_algorithms();
 	
 //---------------------------------------------------------------------
 //Fixed warning: 'int ERR_load_BIO_strings()' is deprecated: Since
@@ -220,15 +236,15 @@ BIO *ssl_setup_bio(void){
 //Previous code was:
 //   ERR_load_BIO_strings();
 //---------------------------------------------------------------------
-	
-    ERR_load_crypto_strings();
-    SSL_load_error_strings();
-    bio = BIO_new(BIO_s_file());
-    bio = BIO_new_fp(stdout, BIO_NOCLOSE);
+	ERR_load_crypto_strings();
+	SSL_load_error_strings();
+	bio = BIO_new(BIO_s_file());
+	bio = BIO_new_fp(stdout, BIO_NOCLOSE);
 	return bio;
 }
 
-void show_cert_data(SSL *ssl, BIO *outbio, const char *hostname) {
+void show_cert_data(SSL *ssl, BIO *outbio, const char *hostname)
+{
 	//Display ssl certificate data here.
 	//Get the remote certificate into the X509 structure
 	printf("--------------------------------------------------------------\n");
@@ -253,18 +269,17 @@ void show_cert_data(SSL *ssl, BIO *outbio, const char *hostname) {
 	printf("--------------------------------------------------------------\n");
 }
 
-void set_to_non_blocking(const int sock){
+void set_to_non_blocking(const int sock)
+{
 	//Set a socket to be non-blocking.
 	int opts;
 	opts = fcntl(sock, F_GETFL);
-	if (opts < 0)
-	{
+	if (opts < 0) {
 		perror("ERROR: fcntl(F_GETFL)");
 		exit(EXIT_FAILURE);
 	}
 	opts = (opts | O_NONBLOCK);
-	if (fcntl(sock, F_SETFL, opts) < 0)
-	{
+	if (fcntl(sock, F_SETFL, opts) < 0) {
 		perror("ERROR: fcntl(O_NONBLOCK)");
 		exit(EXIT_FAILURE);
 	}
